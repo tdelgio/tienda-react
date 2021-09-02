499c11886c9227ce71e9856c9caa8b0aeeb05286
tdelgio <tomas.dg@me.com>
2021-09-01 22:20:05 -0500

    rm border

--- src/components/containers/ItemDetailContainer.js
+++ src/components/containers/ItemDetailContainer.js
@@ -10,17 +10,6 @@ import { getProducts } from "../../utils/promises";
 import ItemDetail from "../ItemDetail";
 import AnimationSpin from "../AnimationSpin";
 
-const ItemsDescription = () => (
-  <ItemDetail
-    key={3}
-    title="title"
-    description="desc"
-    src={`https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg`}
-    price={5}
-    category="both"
-  />
-);
-
 const ItemDetailContainer = () => {
   const [item, setItem] = useState([{}]);
   const [loading, setLoading] = useState(true);
@@ -41,8 +30,8 @@ const ItemDetailContainer = () => {
   console.log(item);
 
   return (
-    <div className="border-2 border-red-500 h-1/2 w-1/2 mx-auto">
-      {loading ? <AnimationSpin /> : <ItemsDescription item={item} />}
+    <div className="h-full w-1/2 mx-auto">
+      {loading ? <AnimationSpin /> : <ItemDetail item={item} />}
     </div>
   );
 };
