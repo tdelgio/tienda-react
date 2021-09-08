6b47536b615c60afc6f7205605c32736d68a0b34
tdelgio <tomas.dg@me.com>
2021-09-08 17:37:23 -0500

    center animation spin

--- src/components/AnimationSpin.js
+++ src/components/AnimationSpin.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const AnimationSpin = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center flex-inline text-gray-600">
+    <div className="min-h-screen flex items-center justify-center flex-inline text-gray-600 transform -translate-y-24">
       <svg
         className="animate-spin -ml-1 mr-3 h-5 w-5 text-gray-500"
         xmlns="http://www.w3.org/2000/svg"
