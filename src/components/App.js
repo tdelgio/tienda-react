6660af4655df0dbf6f2bd0a88a46f05a1b4a2e36
tdelgio <tomas.dg@me.com>
2021-09-23 10:27:42 -0500

    rm browserRouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as HashRouter, Switch, Route } from "react-router-dom";
+import { HashRouter, Switch, Route } from "react-router-dom";
 
 import Layout from "./Layout";
 import ItemListContainer from "./containers/ItemListContainer";
